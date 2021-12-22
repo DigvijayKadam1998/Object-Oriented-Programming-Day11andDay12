137b90df89b5cad2e1bc3da4abce423f153b2e90
[Digvikadam] <[digvikadam168@gmail.com]>
2021-12-22 11:17:35 +0530

    Added: Program to Withdrawn Money From Account

---
+++ src/com/bridgelabz/account/AccountDetails.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.account;
+
+public class AccountDetails {
+	public static void main(String[] args) {
+		AccountTest accounttest = new AccountTest();
+		AccountTest.debit();
+	}
+}
